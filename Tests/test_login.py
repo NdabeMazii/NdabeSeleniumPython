40e74db96531f87b8de953af752b9534878cd20d
NdabeMazii <ndabezinhlemazibuko6@gmail.com>
2026-08-25 12:08:10 +0200

    Added invalid test and handled alert

--- Tests/test_login.py
+++ Tests/test_login.py
@@ -2,7 +2,9 @@ from time import sleep
 
 import allure
 import pytest
-from selenium.webdriver.common import driver_finder
+from selenium.webdriver.common import driver_finder, alert
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from Pages.home_page import home_page
 from Pages.login_page import login_page
@@ -34,7 +36,29 @@ class TestLogin:
         login.getPassword(self.password)
         login.clickLoginButton()
         sleep(10)
-        allure.attach(self.driver.get_screenshot_as_png(), name="Login positive", attachment_type=allure.attachment_type.PNG)
+        allure.attach(self.driver.get_screenshot_as_png(), name="Login positive",
+                      attachment_type=allure.attachment_type.PNG)
+
+    @pytest.mark.test
+    def test_invalid_login(self, setup):
+
+        self.driver = setup
+        self.driver.get(self.dev_url)
+
+        home = home_page(self.driver)
+        home.click_main_login_button()
+
+        login = login_page(self.driver)
+        login.getUsername(self.invalid_username)
+        login.getPassword(self.invalid_password)
+        login.clickLoginButton()
+        wait = WebDriverWait(self.driver, 10)
+        alert = wait.until(EC.alert_is_present())
+        alert.accept()
+        allure.attach(self.driver.get_screenshot_as_png(), name="Login negative",
+                      attachment_type=allure.attachment_type.PNG)
+
+
 
 
 
